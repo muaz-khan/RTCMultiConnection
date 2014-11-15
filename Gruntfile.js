b19cd1c147a5ac85ff674a247d5c5002ab5abcdc
Muaz Khan <muazkh@gmail.com>
2014-11-15 18:35:38 +0500

    Updated for grunt and other stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,15 +32,54 @@ module.exports = function(grunt) {
                 'RTCMultiConnection.min.js': ['RTCMultiConnection.js']
               }
             }
-          }
+          },
+          jsbeautifier : {
+                files : ['RTCMultiConnection.js', 'demos/*.html'],
+                options : {
+                    js: {
+                      braceStyle: "collapse",
+                      breakChainedMethods: false,
+                      e4x: false,
+                      evalCode: false,
+                      indentChar: " ",
+                      indentLevel: 0,
+                      indentSize: 4,
+                      indentWithTabs: false,
+                      jslintHappy: false,
+                      keepArrayIndentation: false,
+                      keepFunctionIndentation: false,
+                      maxPreserveNewlines: 10,
+                      preserveNewlines: true,
+                      spaceBeforeConditional: true,
+                      spaceInParen: false,
+                      unescapeStrings: false,
+                      wrapLineLength: 0
+                  },
+                  html: {
+                      braceStyle: "collapse",
+                      indentChar: " ",
+                      indentScripts: "keep",
+                      indentSize: 4,
+                      maxPreserveNewlines: 10,
+                      preserveNewlines: true,
+                      unformatted: ["a", "sub", "sup", "b", "i", "u"],
+                      wrapLineLength: 0
+                  },
+                  css: {
+                      indentChar: " ",
+                      indentSize: 4
+                  }
+                }
+            }
     });
 
     // enable plugins
+    grunt.loadNpmTasks('grunt-jsbeautifier');
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
+    
     // set default tasks to run when grunt is called without parameters
     // http://gruntjs.com/api/grunt.task
-    grunt.registerTask('default', ['htmlhint', 'jshint', 'uglify']);
+    grunt.registerTask('default', ['jsbeautifier', 'htmlhint', 'jshint', 'uglify']);
 };
