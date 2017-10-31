5447fb8f75226b3d72415c223c26f9fce123d741
MAKIO135 <makio135@gmail.com>
2017-10-31 22:38:17 +0100

    fix

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,60 +1,38 @@
 let socket, socketReady = false;
-console.log( location )
-// UI / buttons events
-document.getElementById('join-room').onclick = function() {
-    location = location.origin + document.getElementById('room-id').value;
 
-    // ......................................................
-    // ......................Handling Room-ID................
-    // ......................................................
+document.querySelector( '#join-room' ).addEventListener( 'click', e => {
+    location = location.origin + document.querySelector( '#room-id' ).value;
 
-    var roomid = '';
-
-    var hashString = location.hash.replace('#', '');
-    if (hashString.length && hashString.indexOf('comment-') == 0) {
-        hashString = '';
-    }
-
-    if (!roomid && hashString.length) {
-        roomid = hashString;
-    }
-
-    if (roomid && roomid.length) {
-        localStorage.setItem(connection.socketMessageEvent, roomid);
+    let roomid = location.hash.replace('#', '');
+    if ( roomid != '' ) {
+        console.log( roomid );
+        localStorage.setItem( connection.socketMessageEvent, roomid );
 
         // auto-join-room
-        (function reCheckRoomPresence() {
-            connection.checkPresence(roomid, function(isRoomExists) {
-                if (isRoomExists) {
-                    connection.join(roomid);
+        ( function reCheckRoomPresence() {
+            console.log( 'reCheckRoomPresence' );
+
+            connection.checkPresence( roomid, isRoomExists => {
+                if ( isRoomExists ) {
+                    connection.join( roomid );
+                    toggleButtons();
                     return;
                 }
 
-                setTimeout(reCheckRoomPresence, 5000);
-            });
-        })();
-
-        disableInputButtons();
+                setTimeout( reCheckRoomPresence, 5000 );
+            } );
+        } )();
     }
-};
+} );
 
-function disableInputButtons() {
-    document.getElementById('room-id').style.display = "none";
-    document.getElementById('join-room').style.display = "none";
-    document.getElementById('txt-cam').style.display = "block";
-    document.getElementById('div-buttons-robot').style.display = "block";
-}
-
-function enableInputButtons() {
-    document.getElementById('room-id').style.display = "auto";
-    document.getElementById('join-room').style.display = "auto";
-    document.getElementById('txt-cam').style.display = "none";
-    document.getElementById('div-buttons-robot').style.display = "none";
+function toggleButtons() {
+    document.querySelector( '#pre-room' ).classList.toggle( 'hidden' );
+    document.querySelector( '#in-room' ).classList.toggle( 'hidden' );
 }
 
 document.querySelectorAll( '.cmd-btn' ).forEach( d => d.addEventListener( 'click', e => {
     console.log( d.dataset.cmd );
-    if(socketReady) socket.emit('cmd', { cmd: d.dataset.cmd } );
+    if( socketReady ) socket.emit( 'cmd', { cmd: d.dataset.cmd } );
 } ) );
 
 
@@ -62,9 +40,9 @@ document.querySelectorAll( '.cmd-btn' ).forEach( d => d.addEventListener( 'click
 // ..................RTCMultiConnection Code.............
 // ......................................................
 
-var connection = new RTCMultiConnection();
+const connection = new RTCMultiConnection();
 
-(function setupRTCConnection(){
+( function setupRTCConnection(){
     // by default, socket.io server is assumed to be deployed on your own URL
     // connection.socketURL = '/';
 
@@ -80,46 +58,46 @@ var connection = new RTCMultiConnection();
         OfferToReceiveVideo: true
     };
 
-    connection.videosContainer = document.getElementById('videos-container');
-    connection.onstream = function(event) {
+    connection.videosContainer = document.getElementById( 'videos-container' );
+    connection.onstream = function( event ) {
         socket = connection.getSocket();
         socketReady = true;
 
-        var width = parseInt(connection.videosContainer.clientWidth / 4) - 20;
-        var mediaElement = getMediaElement(event.mediaElement, {
+        let width = parseInt( connection.videosContainer.clientWidth / 4 ) - 20;
+        let mediaElement = getMediaElement( event.mediaElement, {
             title: event.userid,
-            buttons: ['full-screen'],
+            buttons: [ 'full-screen' ],
             width: width,
             showOnMouseEnter: false
-        });
+        } );
 
-        connection.videosContainer.appendChild(mediaElement);
+        connection.videosContainer.appendChild( mediaElement );
 
-        setTimeout(function() {
+        setTimeout( function() {
             mediaElement.media.play();
         }, 5000);
 
         mediaElement.id = event.streamid;
     };
 
-    connection.onstreamended = function(event) {
-        var mediaElement = document.getElementById(event.streamid);
+    connection.onstreamended = function( event ) {
+        let mediaElement = document.getElementById( event.streamid );
         if (mediaElement) {
-            mediaElement.parentNode.removeChild(mediaElement);
+            mediaElement.parentNode.removeChild( mediaElement );
         }
     };
 
     // to make it one-to-one
     connection.maxParticipantsAllowed = 1;
-    connection.onRoomFull = function(roomid) {
+    connection.onRoomFull = function( roomid ) {
         connection.closeSocket();
-        connection.attachStreams.forEach(function(stream) {
+        connection.attachStreams.forEach( function( stream ) {
             stream.stop();
-        });
+        } );
 
         location = location.origin;
-        enableInputButtons();
+        toggleButtons();
 
-        alert('Room is full.');
+        alert( 'Room is full.' );
     };
-})();
+} )();
