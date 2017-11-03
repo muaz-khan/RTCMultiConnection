41c7f8921e20ca2338da0daeda66d9814b2b6450
MAKIO135 <makio135@gmail.com>
2017-11-03 12:58:40 +0100

    connection.js ES6

--- src/public/js/connection.js
+++ src/public/js/connection.js
@@ -4,61 +4,57 @@
 
 const connection = new RTCMultiConnection();
 
-( function setupRTCConnection(){
-    // by default, socket.io server is assumed to be deployed on your own URL
-    connection.socketURL = '/';
+// by default, socket.io server is assumed to be deployed on your own URL
+connection.socketURL = '/';
 
-    connection.socketMessageEvent = 'video-conference-demo';
+connection.socketMessageEvent = 'video-conference-demo';
 
-    connection.session = {
-        audio: true,
-        video: true
-    };
+connection.session = {
+    audio: true,
+    video: true
+};
 
-    connection.sdpConstraints.mandatory = {
-        OfferToReceiveAudio: true,
-        OfferToReceiveVideo: true
-    };
+connection.sdpConstraints.mandatory = {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true
+};
 
-    connection.videosContainer = document.querySelector('#videos-container');
-    connection.onstream = function( event ) {
-        socket = connection.getSocket();
-        socketReady = true;
+connection.videosContainer = document.querySelector( '#videos-container' );
+connection.onstream = event => {
+    socket = connection.getSocket();
+    socketReady = true;
 
-        // let width = parseInt( connection.videosContainer.clientWidth / 4 ) - 20;
-        let mediaElement = getMediaElement( event.mediaElement, {
-            // title: event.userid,
-            // buttons: [ 'full-screen' ],
-            buttons: [],
-            // width: width,
-            showOnMouseEnter: false
-        } );
+    let mediaElement = getMediaElement( event.mediaElement, {
+        // title: event.userid,
+        // buttons: [ 'full-screen' ],
+        buttons: [],
+        showOnMouseEnter: false
+    } );
 
-        connection.videosContainer.appendChild( mediaElement );
+    connection.videosContainer.appendChild( mediaElement );
 
-        setTimeout( () => mediaElement.media.play(), 5000 );
+    setTimeout( () => mediaElement.media.play(), 5000 );
 
-        mediaElement.id = event.streamid;
-    };
+    mediaElement.id = event.streamid;
+};
 
-    connection.onstreamended = function( event ) {
-        let mediaElement = document.getElementById( event.streamid );
-        if (mediaElement) {
-            mediaElement.parentNode.removeChild( mediaElement );
-        }
-    };
+connection.onstreamended = event => {
+    let mediaElement = document.getElementById( event.streamid );
+    if( mediaElement ) {
+        mediaElement.parentNode.removeChild( mediaElement );
+    }
+};
 
-    // to make it one-to-one
-    connection.maxParticipantsAllowed = 1;
-    connection.onRoomFull = function( roomid ) {
-        connection.closeSocket();
-        connection.attachStreams.forEach( function( stream ) {
-            stream.stop();
-        } );
+// to make it one-to-one
+connection.maxParticipantsAllowed = 1;
+connection.onRoomFull = function( roomid ) {
+    connection.closeSocket();
+    connection.attachStreams.forEach(  stream => {
+        stream.stop();
+    } );
 
-        location = location.origin;
-        toggleButtons();
+    location = location.origin;
+    toggleButtons();
 
-        alert( 'Room is full.' );
-    };
-} )();
+    alert( 'Room is full.' );
+};
