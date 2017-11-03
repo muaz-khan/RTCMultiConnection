066d30313caf1840f4c19f545446e4b8e18d5aff
MAKIO135 <makio135@gmail.com>
2017-11-03 11:06:51 +0100

    getting video back

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -10,8 +10,6 @@ document.querySelector( '#join-room' ).addEventListener( 'click', e => {
 
         // auto-join-room
         ( function reCheckRoomPresence() {
-            console.log( 'reCheckRoomPresence' );
-
             connection.checkPresence( roomid, isRoomExists => {
                 if ( isRoomExists ) {
                     connection.join( roomid );
@@ -34,70 +32,3 @@ document.querySelectorAll( '.cmd-btn' ).forEach( d => d.addEventListener( 'click
     console.log( d.dataset.cmd );
     if( socketReady ) socket.emit( 'cmd', { cmd: d.dataset.cmd } );
 } ) );
-
-
-// ......................................................
-// ..................RTCMultiConnection Code.............
-// ......................................................
-
-const connection = new RTCMultiConnection();
-
-( function setupRTCConnection(){
-    // by default, socket.io server is assumed to be deployed on your own URL
-    // connection.socketURL = '/';
-
-    // connection.socketMessageEvent = 'video-conference-demo';
-
-    connection.session = {
-        audio: true,
-        video: true
-    };
-
-    connection.sdpConstraints.mandatory = {
-        OfferToReceiveAudio: true,
-        OfferToReceiveVideo: true
-    };
-
-    connection.videosContainer = document.getElementById( 'videos-container' );
-    connection.onstream = function( event ) {
-        socket = connection.getSocket();
-        socketReady = true;
-
-        let width = parseInt( connection.videosContainer.clientWidth / 4 ) - 20;
-        let mediaElement = getMediaElement( event.mediaElement, {
-            title: event.userid,
-            buttons: [ 'full-screen' ],
-            width: width,
-            showOnMouseEnter: false
-        } );
-
-        connection.videosContainer.appendChild( mediaElement );
-
-        setTimeout( function() {
-            mediaElement.media.play();
-        }, 5000);
-
-        mediaElement.id = event.streamid;
-    };
-
-    connection.onstreamended = function( event ) {
-        let mediaElement = document.getElementById( event.streamid );
-        if (mediaElement) {
-            mediaElement.parentNode.removeChild( mediaElement );
-        }
-    };
-
-    // to make it one-to-one
-    connection.maxParticipantsAllowed = 1;
-    connection.onRoomFull = function( roomid ) {
-        connection.closeSocket();
-        connection.attachStreams.forEach( function( stream ) {
-            stream.stop();
-        } );
-
-        location = location.origin;
-        toggleButtons();
-
-        alert( 'Room is full.' );
-    };
-} )();
--- src/public/js/main_robot.js
+++ src/public/js/main_robot.js
@@ -1,57 +1,32 @@
+// mobileConsole
+addEventListener( 'load', e => {
+    mobileConsole.init();
+} );
+
 // Blue web
-// Obtain configured Bluetooth Terminal instance
 let terminal = new BluetoothTerminal();
 
-// Override `receive` method to log incoming data to the terminal
-terminal.receive = function(data) {
-    console.log(data, 'in');
+terminal.receive = data => {
+    console.log( data, 'in' );
 };
 
-// Override default log method to output messages to the terminal and console
-terminal._log = function(...messages) {
-    // We cannot use `super._log()` here
-    messages.forEach(message => {
-        let p = document.createElement('p')
+terminal._log = ( ...messages ) => {
+    messages.forEach( message => {
+        let p = document.createElement( 'p' );
         p.innerText = message;
         document.body.appendChild(p);
-    });
+    } );
 };
 
-// Bind event listeners to the UI elements
-// let connectButton = document.getElementById('connect');
-// connectButton.addEventListener('click', function() {
-//     terminal.connect().then(() => {
-//         console.log( `connected to ${terminal.getDeviceName()}`)
-//     });
-// });
-
-// let disconnectButton = document.getElementById('disconnect');
-// disconnectButton.addEventListener('click', function() {
-//     terminal.disconnect();
-//     console.log( 'disconnected' );
-// });
-
-// Implement own send function to log outcoming data to the terminal
-// function send(data) {
-//     terminal.send(data).then(() => console.log(data, 'out')).catch(error => console.log(error));
-// }
-// document.querySelectorAll( '.cmd' ).forEach( d => d.addEventListener( 'click', e => {
-//     console.log( d.dataset.cmd );
-//     send( d.dataset.cmd );
-// } ) );
-
-addEventListener('load', e => {
-    mobileConsole.init();
-});
 
 
 // UI / buttons events
-document.querySelector('#open-room').addEventListener( 'click', function() {
+document.querySelector( '#open-room' ).addEventListener( 'click', function() {
     // terminal.connect().then(() => {
     //     console.log( `connected to ${terminal.getDeviceName()}`)
     // });
 
-    connection.open(document.getElementById('room-id').value, function() {
+    connection.open( document.querySelector('#room-id').value, function() {
         showRoomURL(connection.sessionid);
 
 
@@ -69,113 +44,64 @@ document.querySelector('#open-room').addEventListener( 'click', function() {
 });
 
 function disableInputButtons() {
-    document.getElementById('open-room').style.display = "none";
-    document.getElementById('room-id').style.display = "none"
-    document.getElementById('txt-cam').style.display = "block";
+    document.querySelector('#open-room').style.display = "none";
+    document.querySelector('#room-id').style.display = "none"
+    document.querySelector('#txt-cam').style.display = "block";
 }
-
-// ......................................................
-// ..................RTCMultiConnection Code.............
-// ......................................................
-
-var connection = new RTCMultiConnection();
-
-// by default, socket.io server is assumed to be deployed on your own URL
-connection.socketURL = '/';
-
-connection.socketMessageEvent = 'video-conference-demo';
-
-connection.session = {
-    audio: true,
-    video: true
-};
-
-connection.sdpConstraints.mandatory = {
-    OfferToReceiveAudio: true,
-    OfferToReceiveVideo: true
-};
-
-connection.videosContainer = document.getElementById('videos-container');
-connection.onstream = function(event) {
-    var width = parseInt(connection.videosContainer.clientWidth / 2) - 20;
-    var mediaElement = getMediaElement(event.mediaElement, {
-        title: event.userid,
-        buttons: ['full-screen'],
-        width: width,
-        showOnMouseEnter: false
-    });
-
-    connection.videosContainer.appendChild(mediaElement);
-
-    setTimeout(function() {
-        mediaElement.media.play();
-    }, 5000);
-
-    mediaElement.id = event.streamid;
-};
-
-connection.onstreamended = function(event) {
-    var mediaElement = document.getElementById(event.streamid);
-    if (mediaElement) {
-        mediaElement.parentNode.removeChild(mediaElement);
-    }
-};
 
 // ......................................................
 // ......................Handling Room-ID................
 // ......................................................
 
 function showRoomURL(roomid) {
-    var roomHashURL = '#' + roomid;
-    var roomQueryStringURL = '?roomid=' + roomid;
+    let roomHashURL = '#' + roomid;
+    let roomQueryStringURL = '?roomid=' + roomid;
 
-    var html = '<h2>Unique URL to connect to the robot:</h2>';
-
-    html += 'Hash URL: ' + 'https://makerschat.herokuapp.com/' + roomHashURL + '</a>';
+    let html = 'Hash URL: ' + 'https://makerschat.herokuapp.com/' + roomHashURL + '</a>';
     html += '<br>';
     html += 'QueryString URL: ' + 'https://makerschat.herokuapp.com/' + roomQueryStringURL + '</a>';
 
-    var roomURLsDiv = document.getElementById('room-urls');
+    let roomURLsDiv = document.querySelector('#room-urls');
     roomURLsDiv.innerHTML = html;
 
     roomURLsDiv.style.display = 'block';
 }(function() {
-    var params = {},
+    let params = {},
         r = /([^&=]+)=?([^&]*)/g;
 
     function d(s) {
         return decodeURIComponent(s.replace(/\+/g, ' '));
     }
-    var match,
+    let match,
         search = window.location.search;
     while (match = r.exec(search.substring(1)))
         params[d(match[1])] = d(match[2]);
     window.params = params;
 })();
 
-var roomid = '';
+let roomid = '';
 if (localStorage.getItem(connection.socketMessageEvent)) {
     roomid = localStorage.getItem(connection.socketMessageEvent);
 } else {
     roomid = connection.token();
 }
-document.getElementById('room-id').value = roomid;
-document.getElementById('room-id').onkeyup = function() {
+document.querySelector('#room-id').value = roomid;
+document.querySelector('#room-id').onkeyup = function() {
     localStorage.setItem(connection.socketMessageEvent, this.value);
 };
 
-var hashString = location.hash.replace('#', '');
+let hashString = location.hash.replace('#', '');
 if (hashString.length && hashString.indexOf('comment-') == 0) {
     hashString = '';
 }
 
-var roomid = params.roomid;
+// let roomid = params.roomid;
 if (!roomid && hashString.length) {
     roomid = hashString;
 }
 
 if (roomid && roomid.length) {
-    document.getElementById('room-id').value = roomid;
+    document.querySelector('#room-id').value = roomid;
     localStorage.setItem(connection.socketMessageEvent, roomid);
 
     // auto-join-room
@@ -193,16 +119,3 @@ if (roomid && roomid.length) {
     disableInputButtons();
 
 }
-
-// to make it one-to-one
-connection.maxParticipantsAllowed = 1;
-connection.onRoomFull = function(roomid) {
-    connection.closeSocket();
-    connection.attachStreams.forEach(function(stream) {
-        stream.stop();
-    });
-
-    enableInputButtons();
-
-    alert('Room is full.');
-};
