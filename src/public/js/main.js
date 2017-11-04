66d08c2c35764825c59781d1e84ebe54ea6a3cb3
nwercto <nico@wercto.com>
2017-11-04 11:48:28 +0100

    Review CSS design

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,55 +1,70 @@
-document.querySelector( '#join-room' ).addEventListener( 'click', e => {
+$(document).ready(function () {
+    $('select').material_select();
+});
+
+function getSelectedText(elementId) {
+    var elt = document.getElementById(elementId);
+
+    if (elt.selectedIndex == -1)
+        return null;
+
+    return elt.options[elt.selectedIndex].text;
+}
+
+document.querySelector('#join-room').addEventListener('click', e => {
     let socket, socketReady = false;
 
-    location = location.origin + '/#' + document.querySelector( '#room-id' ).value;
+    location = location.origin + '/#' + document.querySelector('#room-id').value;
+    document.getElementById('robotName').innerHTML = 'Robot ' + getSelectedText('room-id');
 
     let roomid = location.hash.replace('#', '');
-    if ( roomid != '' ) {
-        console.log( roomid );
-        localStorage.setItem( connection.socketMessageEvent, roomid );
+    if (roomid != '') {
+        console.log(roomid);
+        localStorage.setItem(connection.socketMessageEvent, roomid);
 
         // auto-join-room
-        ( function reCheckRoomPresence() {
-            connection.checkPresence( roomid, isRoomExists => {
-                if ( isRoomExists ) {
-                    connection.join( roomid, () => {
-                        console.log( 'connection joined' );
-                        connection.getSocket( _socket => {
-                            console.log( 'get socket' );
+        (function reCheckRoomPresence() {
+            connection.checkPresence(roomid, isRoomExists => {
+                if (isRoomExists) {
+                    connection.join(roomid, () => {
+                        console.log('connection joined');
+                        connection.getSocket(_socket => {
+                            console.log('get socket');
                             socket = _socket;
                             socketReady = true;
-                            socket.on( 'cmd', data => console.log( data ) );
-                        } );
-                    } );
+                            socket.on('cmd', data => console.log(data));
+                        });
+                    });
                     toggleButtons();
                     return;
                 }
 
-                setTimeout( reCheckRoomPresence, 5000 );
-            } );
-        } )();
+                setTimeout(reCheckRoomPresence, 5000);
+            });
+        })();
     }
 
     function toggleButtons() {
-        document.querySelector( '#pre-room' ).classList.toggle( 'hidden' );
-        document.querySelector( '#in-room' ).classList.toggle( 'hidden' );
+        document.querySelector('#pre-room').classList.toggle('hidden');
+        document.querySelector('#in-room').classList.toggle('hidden');
+        document.querySelector('#mainContainer').classList.toggle('bottom');
     }
 
     let cmd = '';
-    document.querySelectorAll( '.cmd-btn' ).forEach( d => {
-        d.addEventListener( 'mousedown', e => {
+    document.querySelectorAll('.cmd-btn').forEach(d => {
+        d.addEventListener('mousedown', e => {
             cmd = d.dataset.cmd;
             sendCmd();
-        } );
-        d.addEventListener( 'mouseup', e => {
+        });
+        d.addEventListener('mouseup', e => {
             cmd = '';
-        } );
-    } );
+        });
+    });
 
-    function sendCmd(){
-        if( cmd != '' && socketReady ){
-            socket.emit( 'cmd', { roomid, cmd } );
-            setTimeout( sendCmd, 500 );
+    function sendCmd() {
+        if (cmd != '' && socketReady) {
+            socket.emit('cmd', { roomid, cmd });
+            setTimeout(sendCmd, 500);
         }
     }
-} );
+});
--- src/public/js/main_robot.js
+++ src/public/js/main_robot.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+    $('select').material_select();
+});
+
+function getSelectedText(elementId) {
+    var elt = document.getElementById(elementId);
+
+    if (elt.selectedIndex == -1)
+        return null;
+
+    return elt.options[elt.selectedIndex].text;
+}
+
 // mobileConsole
 // addEventListener( 'load', e => {
 //     mobileConsole.init();
@@ -23,22 +36,25 @@ terminal._log = ( ...messages ) => {
 document.querySelector( '#open-room' ).addEventListener( 'click', () => {
     let roomid = document.querySelector( '#room-id' ).value;
 
+    document.getElementById('robotName').innerHTML = 'Robot ' + getSelectedText('room-id');
+
     localStorage.setItem( connection.socketMessageEvent, roomid );
 
-    // terminal.connect().then( () => {
-    //     console.log( `connected to ${ terminal.getDeviceName() }` );
-    // } );
+    /*terminal.connect().then( () => {
+        console.log( `connected to ${ terminal.getDeviceName() }` );
+    } );*/
 
     connection.open( roomid, () => {
         document.querySelector( '#pre-room' ).classList.toggle( 'hidden' );
         document.querySelector( '#in-room' ).classList.toggle( 'hidden' );
+        document.querySelector('#mainContainer').classList.toggle('bottom');
         console.log( `Join bot here: https://makerschat.herokuapp.com/#${ connection.sessionid }` );
 
         connection.getSocket( socket => {
             socket.on( 'cmd', data => {
                 console.log( data );
                 if( data.roomid === roomid ){
-                    // terminal.send( data.cmd ).then( () => console.log( data, 'out' ) ).catch( error => console.log( error ) );
+                    //terminal.send( data.cmd ).then( () => console.log( data, 'out' ) ).catch( error => console.log( error ) );
                 }
             } );
         } );
