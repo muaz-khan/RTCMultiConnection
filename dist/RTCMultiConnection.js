bb55a0506771116d00bbb806048126fccc98bc69
Muaz Khan <muazkh@gmail.com>
2017-11-19 19:13:08 +0500

    Updated to latest DetectRTC.js

--- dist/RTCMultiConnection.js
+++ dist/RTCMultiConnection.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Last time updated: 2017-11-19 1:34:05 PM UTC
+// Last time updated: 2017-11-19 2:11:44 PM UTC
 
 // _________________________
 // RTCMultiConnection v3.4.4
@@ -891,10 +891,10 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
 
     'use strict';
 
-    // Last Updated On: 2017-08-31 6:52:39 AM UTC
+    // Last Updated On: 2017-11-19 2:09:51 PM UTC
 
     // ________________
-    // DetectRTC v1.3.5
+    // DetectRTC v1.3.6
 
     // Open-Sourced: https://github.com/muaz-khan/DetectRTC
 
@@ -972,7 +972,7 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
 
         var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
         var isFirefox = typeof window.InstallTrigger !== 'undefined';
-        var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+        var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
         var isChrome = !!window.chrome && !isOpera;
         var isIE = typeof document !== 'undefined' && !!document.documentMode && !isEdge;
 
@@ -1019,11 +1019,16 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
             // In Safari, the true version is after 'Safari' or after 'Version' 
             else if (isSafari) {
                 verOffset = nAgt.indexOf('Safari');
+
                 browserName = 'Safari';
                 fullVersion = nAgt.substring(verOffset + 7);
 
                 if ((verOffset = nAgt.indexOf('Version')) !== -1) {
                     fullVersion = nAgt.substring(verOffset + 8);
+                }
+
+                if (navigator.userAgent.indexOf('Version/') !== -1) {
+                    fullVersion = navigator.userAgent.split('Version/')[1].split(' ')[0];
                 }
             }
             // In Firefox, the true version is after 'Firefox' 
@@ -1397,98 +1402,77 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
         });
 
         // via: https://github.com/diafygi/webrtc-ips
-        function DetectLocalIPAddress(callback) {
+        function DetectLocalIPAddress(callback, stream) {
             if (!DetectRTC.isWebRTCSupported) {
                 return;
             }
 
-            if (DetectRTC.isORTCSupported) {
-                return;
-            }
-
             getIPs(function(ip) {
-                //local IPs
                 if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/)) {
                     callback('Local: ' + ip);
-                }
-
-                //assume the rest are public IPs
-                else {
+                } else {
                     callback('Public: ' + ip);
                 }
-            });
+            }, stream);
         }
 
-        //get the IP addresses associated with an account
-        function getIPs(callback) {
+        function getIPs(callback, stream) {
             if (typeof document === 'undefined' || typeof document.getElementById !== 'function') {
                 return;
             }
 
             var ipDuplicates = {};
 
-            //compatibility for firefox and chrome
             var RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-            var useWebKit = !!window.webkitRTCPeerConnection;
 
-            // bypass naive webrtc blocking using an iframe
             if (!RTCPeerConnection) {
                 var iframe = document.getElementById('iframe');
                 if (!iframe) {
-                    //<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
-                    throw 'NOTE: you need to have an iframe in the page right above the script tag.';
+                    return;
                 }
                 var win = iframe.contentWindow;
                 RTCPeerConnection = win.RTCPeerConnection || win.mozRTCPeerConnection || win.webkitRTCPeerConnection;
-                useWebKit = !!win.webkitRTCPeerConnection;
             }
 
-            // if still no RTCPeerConnection then it is not supported by the browser so just return
             if (!RTCPeerConnection) {
                 return;
             }
 
-            //minimal requirements for data connection
-            var mediaConstraints = {
-                optional: [{
-                    RtpDataChannels: true
+            var peerConfig = null;
+
+            if (DetectRTC.browser === 'Chrome' && DetectRTC.browser.version < 58) {
+                // todo: add support for older Opera
+                peerConfig = {
+                    optional: [{
+                        RtpDataChannels: true
+                    }]
+                };
+            }
+
+            var servers = {
+                iceServers: [{
+                    urls: 'stun:stun.l.google.com:19302'
                 }]
             };
 
-            //firefox already has a default stun server in about:config
-            //    media.peerconnection.default_iceservers =
-            //    [{"url": "stun:stun.services.mozilla.com"}]
-            var servers;
+            var pc = new RTCPeerConnection(servers, peerConfig);
 
-            //add same stun server for chrome
-            if (useWebKit) {
-                servers = {
-                    iceServers: [{
-                        urls: 'stun:stun.services.mozilla.com'
-                    }]
-                };
-
-                if (typeof DetectRTC !== 'undefined' && DetectRTC.browser.isFirefox && DetectRTC.browser.version <= 38) {
-                    servers[0] = {
-                        url: servers[0].urls
-                    };
+            if (stream) {
+                if (pc.addStream) {
+                    pc.addStream(stream);
+                } else if (pc.addTrack && stream.getTracks()[0]) {
+                    pc.addTrack(stream.getTracks()[0], stream);
                 }
             }
 
-            //construct a new RTCPeerConnection
-            var pc = new RTCPeerConnection(servers, mediaConstraints);
-
             function handleCandidate(candidate) {
-                //match just the IP address
                 var ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/;
                 var match = ipRegex.exec(candidate);
                 if (!match) {
-                    console.warn('Could not match IP address in', candidate);
                     return;
                 }
                 var ipAddress = match[1];
 
-                //remove duplicates
                 if (ipDuplicates[ipAddress] === undefined) {
                     callback(ipAddress);
                 }
@@ -1496,28 +1480,32 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
                 ipDuplicates[ipAddress] = true;
             }
 
-            //listen for candidate events
+            // listen for candidate events
             pc.onicecandidate = function(ice) {
-                //skip non-candidate events
                 if (ice.candidate) {
                     handleCandidate(ice.candidate.candidate);
                 }
             };
 
-            //create a bogus data channel
-            pc.createDataChannel('');
+            // create data channel
+            if (!stream) {
+                try {
+                    pc.createDataChannel('sctp', {});
+                } catch (e) {}
+            }
 
-            //create an offer sdp
-            pc.createOffer(function(result) {
+            // create an offer sdp
+            if (DetectRTC.isPromisesSupported) {
+                pc.createOffer().then(function(result) {
+                    pc.setLocalDescription(result).then(afterCreateOffer);
+                });
+            } else {
+                pc.createOffer(function(result) {
+                    pc.setLocalDescription(result, afterCreateOffer, function() {});
+                }, function() {});
+            }
 
-                //trigger the stun server request
-                pc.setLocalDescription(result, function() {}, function() {});
-
-            }, function() {});
-
-            //wait for a while to let everything done
-            setTimeout(function() {
-                //read candidate info from local description
+            function afterCreateOffer() {
                 var lines = pc.localDescription.sdp.split('\n');
 
                 lines.forEach(function(line) {
@@ -1525,7 +1513,7 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
                         handleCandidate(line);
                     }
                 });
-            }, 1000);
+            }
         }
 
         var MediaDevices = [];
@@ -1538,9 +1526,14 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
             // Firefox 38+ seems having support of enumerateDevices
             // Thanks @xdumaine/enumerateDevices
             navigator.enumerateDevices = function(callback) {
-                navigator.mediaDevices.enumerateDevices().then(callback).catch(function() {
+                var enumerateDevices = navigator.mediaDevices.enumerateDevices();
+                if (enumerateDevices && enumerateDevices.then) {
+                    navigator.mediaDevices.enumerateDevices().then(callback).catch(function() {
+                        callback([]);
+                    });
+                } else {
                     callback([]);
-                });
+                }
             };
         }
 
@@ -1865,9 +1858,15 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
         DetectRTC.checkWebSocketsSupport = function(callback) {
             callback = callback || function() {};
             try {
+                var starttime;
                 var websocket = new WebSocket('wss://echo.websocket.org:443/');
                 websocket.onopen = function() {
                     DetectRTC.isWebSocketsBlocked = false;
+                    starttime = (new Date).getTime();
+                    websocket.send('ping');
+                };
+                websocket.onmessage = function() {
+                    DetectRTC.WebsocketLatency = (new Date).getTime() - starttime + 'ms';
                     callback();
                     websocket.close();
                     websocket = null;
